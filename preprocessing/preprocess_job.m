63f5e4b633aebeaed7de8fef0bd9d0a91cd5aed3
ArunavDey <arunavdey01@gmail.com>
2022-09-16 10:37:37 +0530

    preprocessing: add code

---
+++ preprocessing/preprocess_job.m
@@ -0,0 +1,135 @@
+% Runs the preprocessing pipeline
+% Authors:  Arunav Dey, Jigya Singh, Manaswini Rathore, Roshni Govind
+
+spm_jobman('initcfg');
+
+% subs = [04856 15213 20732 29935 38088 46856 54541 58313 61001 61241 66941 70752 84978 88286 91196 96705]; % Palo Alto
+subs = [04856];
+
+% subs = [02248 07210 13304 16112 17516 23740 24528 40095 40451 47141 48778 56945 57368 66945 68195 71633 74429 76621 79611 80089 82071 85152]; % Oxford
+
+rootdir = '/home/arunav/Assets/Capstone/PaloAlto';
+% rootdir = '/home/arunav/Assets/Capstone/Oxford';
+
+spmdir = '/home/arunav/matlab/spm12';
+
+for sub = subs
+    sub = num2str(sub, '%05d');
+
+    if exist([rootdir '/sub' sub '/func/rest.nii']) == 0
+        gunzip([rootdir '/sub' sub '/func/rest.nii.gz']);
+    else
+    end
+
+    if exist([rootdir '/sub' sub '/anat/mprage_anonymized.nii']) == 0
+        gunzip([rootdir '/sub' sub '/anat/mprage_anonymized.nii.gz']);
+    else
+    end
+
+    % Selecting the .nii file
+    matlabbatch{1}.cfg_basicio.file_dir.file_ops.cfg_named_file.name = 'restfmri';
+    matlabbatch{1}.cfg_basicio.file_dir.file_ops.cfg_named_file.files = {{[rootdir '/sub' sub '/func/rest.nii']}};
+
+    % Realignment
+    matlabbatch{2}.spm.spatial.realign.estwrite.data{1}(1) = cfg_dep('Named File Selector: restfmri(1) - Files', substruct('.', 'val', '{}', {1}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('.', 'files', '{}', {1}));
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.quality = 0.9;
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.sep = 4;
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.fwhm = 5;
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.rtm = 1;
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.interp = 2;
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.wrap = [0 0 0];
+    matlabbatch{2}.spm.spatial.realign.estwrite.eoptions.weight = '';
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.which = [2 1];
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.interp = 4;
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.wrap = [0 0 0];
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.mask = 1;
+    matlabbatch{2}.spm.spatial.realign.estwrite.roptions.prefix = 'r';
+
+    % Slice time correction
+    matlabbatch{3}.spm.temporal.st.scans{1}(1) = cfg_dep('Realign: Estimate & Reslice: Resliced Images (Sess 1)', substruct('.', 'val', '{}', {2}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('.', 'sess', '()', {1}, '.', 'rfiles'));
+    matlabbatch{3}.spm.temporal.st.nslices = 29;
+    matlabbatch{3}.spm.temporal.st.tr = 2;
+    matlabbatch{3}.spm.temporal.st.ta = 1.93103448275862;
+    matlabbatch{3}.spm.temporal.st.so = [1 3 5 7 9 11 13 15 17 19 21 23 25 27 29 2 4 6 8 10 12 14 16 18 20 22 24 26 28];
+    matlabbatch{3}.spm.temporal.st.refslice = 1;
+    matlabbatch{3}.spm.temporal.st.prefix = 'a';
+
+    % Coregistration
+    matlabbatch{4}.spm.spatial.coreg.estwrite.ref(1) = cfg_dep('Realign: Estimate & Reslice: Mean Image', substruct('.', 'val', '{}', {2}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('.', 'rmean'));
+    matlabbatch{4}.spm.spatial.coreg.estwrite.source = {[rootdir '/sub' sub '/anat/mprage_skullstripped.nii,1']};
+    matlabbatch{4}.spm.spatial.coreg.estwrite.other = {''};
+    matlabbatch{4}.spm.spatial.coreg.estwrite.eoptions.cost_fun = 'nmi';
+    matlabbatch{4}.spm.spatial.coreg.estwrite.eoptions.sep = [4 2];
+    matlabbatch{4}.spm.spatial.coreg.estwrite.eoptions.tol = [0.02 0.02 0.02 0.001 0.001 0.001 0.01 0.01 0.01 0.001 0.001 0.001];
+    matlabbatch{4}.spm.spatial.coreg.estwrite.eoptions.fwhm = [7 7];
+    matlabbatch{4}.spm.spatial.coreg.estwrite.roptions.interp = 4;
+    matlabbatch{4}.spm.spatial.coreg.estwrite.roptions.wrap = [0 0 0];
+    matlabbatch{4}.spm.spatial.coreg.estwrite.roptions.mask = 0;
+    matlabbatch{4}.spm.spatial.coreg.estwrite.roptions.prefix = 'r';
+
+    % Tissue segmentation
+    matlabbatch{5}.spm.spatial.preproc.channel.vols(1) = cfg_dep('Coregister: Estimate & Reslice: Coregistered Images', substruct('.', 'val', '{}', {4}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('.', 'cfiles'));
+    matlabbatch{5}.spm.spatial.preproc.channel.biasreg = 0.001;
+    matlabbatch{5}.spm.spatial.preproc.channel.biasfwhm = 60;
+    matlabbatch{5}.spm.spatial.preproc.channel.write = [0 1];
+    matlabbatch{5}.spm.spatial.preproc.tissue(1).tpm = {[spmdir '/tpm/TPM.nii,1']};
+    matlabbatch{5}.spm.spatial.preproc.tissue(1).ngaus = 1;
+    matlabbatch{5}.spm.spatial.preproc.tissue(1).native = [1 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(1).warped = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(2).tpm = {[spmdir '/tpm/TPM.nii,2']};
+    matlabbatch{5}.spm.spatial.preproc.tissue(2).ngaus = 1;
+    matlabbatch{5}.spm.spatial.preproc.tissue(2).native = [1 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(2).warped = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(3).tpm = {[spmdir '/tpm/TPM.nii,3']};
+    matlabbatch{5}.spm.spatial.preproc.tissue(3).ngaus = 2;
+    matlabbatch{5}.spm.spatial.preproc.tissue(3).native = [1 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(3).warped = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(4).tpm = {[spmdir '/tpm/TPM.nii,4']};
+    matlabbatch{5}.spm.spatial.preproc.tissue(4).ngaus = 3;
+    matlabbatch{5}.spm.spatial.preproc.tissue(4).native = [1 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(4).warped = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(5).tpm = {[spmdir '/tpm/TPM.nii,5']};
+    matlabbatch{5}.spm.spatial.preproc.tissue(5).ngaus = 4;
+    matlabbatch{5}.spm.spatial.preproc.tissue(5).native = [1 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(5).warped = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(6).tpm = {[spmdir '/tpm/TPM.nii,6']};
+    matlabbatch{5}.spm.spatial.preproc.tissue(6).ngaus = 2;
+    matlabbatch{5}.spm.spatial.preproc.tissue(6).native = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.tissue(6).warped = [0 0];
+    matlabbatch{5}.spm.spatial.preproc.warp.mrf = 1;
+    matlabbatch{5}.spm.spatial.preproc.warp.cleanup = 1;
+    matlabbatch{5}.spm.spatial.preproc.warp.reg = [0 0.001 0.5 0.05 0.2];
+    matlabbatch{5}.spm.spatial.preproc.warp.affreg = 'mni';
+    matlabbatch{5}.spm.spatial.preproc.warp.fwhm = 0;
+    matlabbatch{5}.spm.spatial.preproc.warp.samp = 3;
+    matlabbatch{5}.spm.spatial.preproc.warp.write = [0 1];
+    matlabbatch{5}.spm.spatial.preproc.warp.vox = NaN;
+    matlabbatch{5}.spm.spatial.preproc.warp.bb = [NaN NaN NaN
+                                            NaN NaN NaN];
+
+    % Normalisation
+    matlabbatch{6}.spm.spatial.normalise.write.subj.def(1) = cfg_dep('Segment: Forward Deformations', substruct('.', 'val', '{}', {5}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('.', 'fordef', '()', {':'}));
+    matlabbatch{6}.spm.spatial.normalise.write.subj.resample(1) = cfg_dep('Slice Timing: Slice Timing Corr. Images (Sess 1)', substruct('.', 'val', '{}', {3}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('()', {1}, '.', 'files'));
+    matlabbatch{6}.spm.spatial.normalise.write.woptions.bb = [-78 -112 -70
+                                                        78 76 85];
+    matlabbatch{6}.spm.spatial.normalise.write.woptions.vox = [2 2 2];
+    matlabbatch{6}.spm.spatial.normalise.write.woptions.interp = 4;
+    matlabbatch{6}.spm.spatial.normalise.write.woptions.prefix = 'w';
+
+    % Smoothing
+    matlabbatch{7}.spm.spatial.smooth.data(1) = cfg_dep('Normalise: Write: Normalised Images (Subj 1)', substruct('.', 'val', '{}', {6}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('()', {1}, '.', 'files'));
+    matlabbatch{7}.spm.spatial.smooth.fwhm = [6 6 6];
+    matlabbatch{7}.spm.spatial.smooth.dtype = 0;
+    matlabbatch{7}.spm.spatial.smooth.im = 0;
+    matlabbatch{7}.spm.spatial.smooth.prefix = 's';
+
+    % Output files
+    matlabbatch{8}.cfg_basicio.file_dir.file_ops.cfg_file_split.name = 'preproc';
+    matlabbatch{8}.cfg_basicio.file_dir.file_ops.cfg_file_split.files(1) = cfg_dep('Smooth: Smoothed Images', substruct('.', 'val', '{}', {7}, '.', 'val', '{}', {1}, '.', 'val', '{}', {1}), substruct('.', 'files'));
+    matlabbatch{8}.cfg_basicio.file_dir.file_ops.cfg_file_split.index = {
+                                                                    1
+                                                                    2
+                                                                    }';
+    spm_jobman('run', matlabbatch);
+
+end
--- preprocessing/test.py
+++ preprocessing/test.py
@@ -2,13 +2,17 @@ import json
 import os
 from preprocess import preprocess
 from utils import getFilePaths, loadData, saveAsPNG, deleteDir
+import nibabel as nib
 
 with open('../filepath.json', 'r') as f:
     data = f.read()
     paths = json.loads(data)
 
-filepaths = getFilePaths(paths['data'], flag=1) # all .nii files inside PaloAlto
+filepaths = getFilePaths(paths['data'], flag=2) # all .nii files inside PaloAlto
 
-smri = filepaths[0]
-preproc = preprocess(smri, 1, paths['outputs'])
-preproc.run()
+fmri = filepaths[0]
+func = nib.load(fmri)
+header = func.header
+print(header.get_zooms())
+# preproc = preprocess(smri, 1, paths['outputs'])
+# preproc.run()
