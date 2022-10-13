8e582a5753d3b3eea3ac382f2f7c30454367fa54
ArunavDey <arunavdey01@gmail.com>
2022-10-13 17:54:41 +0530

    clean up code

--- feature-extraction/densities.py
+++ feature-extraction/densities.py
@@ -2,16 +2,16 @@ import nibabel as nib
 import numpy as np
 import os
 
-def density(path):
+def density(path, sub):
     segments = list()
 
-    gm_path = os.path.join(path, "c1anat_X_1018959_classify_stereolin.nii")
+    gm_path = os.path.join(path, f"c1anat_X_{sub}_classify_stereolin.nii")
     segments.append(nib.load(gm_path).get_fdata()) 
-    wm_path = os.path.join(path, "c2anat_X_1018959_classify_stereolin.nii")
+    wm_path = os.path.join(path, f"c2anat_X_{sub}_classify_stereolin.nii")
     segments.append(nib.load(wm_path).get_fdata()) 
-    csf_path = os.path.join(path, "c3anat_X_1018959_classify_stereolin.nii")
+    csf_path = os.path.join(path, f"c3anat_X_{sub}_classify_stereolin.nii")
     segments.append(nib.load(csf_path).get_fdata()) 
-    empty_path = os.path.join(path, "c6anat_X_1018959_classify_stereolin.nii")
+    empty_path = os.path.join(path, f"c6anat_X_{sub}_classify_stereolin.nii")
     segments.append(nib.load(empty_path).get_fdata())
 
     res = list()
@@ -23,22 +23,16 @@ def density(path):
         light = np.sum(s > 0)
         res.append(light/(dark+light))
 
-    # total = sum(res)
-    # ratio = 1 / total
-
-    # res[0] *= ratio
-    # res[1] *= ratio
-    # res[2] *= ratio
-
     return res
     
 
 if __name__ == "__main__":
     print("Densities")
     home = "/home/arunav"
-    path = os.path.join(home, "Assets", "test_data", "segmented_1018959_anat")
+    sub = "1018959"
+    path = os.path.join(home, "Assets", "test_data", f"segmented_{sub}_anat")
 
-    gm, wm, csf, _ = density(path)
+    gm, wm, csf, _ = density(path, sub)
     print(f"GM Density: {gm}")
     print(f"WM Density: {wm}")
     print(f"CSF Density: {csf}")
